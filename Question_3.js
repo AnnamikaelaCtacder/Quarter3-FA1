491b6c6de6e8d3fd24e0dbe879f4c5225c397743
AnnamikaelaCtacder <annamikaela.tacder@smc.pshs.edu.ph>
2025-01-29 20:35:38 +0800

    Update Question_3.js

--- Question_3.js
+++ Question_3.js
@@ -2,14 +2,14 @@ var nickname = prompt("Enter your nickname:");
 var heightInInches = parseFloat(prompt("Enter your height (in inches)", "0"));
 var weightInKg = parseFloat(prompt("Enter your weight (in kilograms)", "0"));
 
-var weightInKgfinal = (weightInKg * 2.205).toFixed(3);
+var weightInLbs = (weightInKg * 2.20462).toFixed(3);
 var remainder = heightInInches % 12;
 var heightInFeet = Math.floor(heightInInches / 12);
 
 if (confirm("Do you agree to share your information with our site?")) {
    console.log("Name: " + nickname);
-   console.log("Height: " + heightInFeet + "'" + remainder + "''");
-   console.log("Weight: " + weightInKgfinal + " lbs");
+   console.log("Height: " + heightInFeet + "'" + remainder + "\"");
+   console.log("Weight: " + weightInLbs + " lbs");
 } else {
-   console.log("User does not wish to share their information.");
+   console.log("User does not wish to share his/her information.");
 }
